87d72b53e452c47b6710bc27eaccf706ac7c710d
Rahmin Pavlovic <rpavlovic@northpointdigital.com>
2015-02-02 13:10:12 -0500

    Export and gzip WP site

--- export.sh
+++ export.sh
@@ -2,6 +2,6 @@
 
 mysqldump -uwp_admin -p --add-locks --skip-extended-insert --skip-quick --databases wordpress | cat > wp-$(date +%Y-%m-%d-%H.%M.%S).sql;
 
-gzip -c *.php wp-* license.txt *.html > wp_site.tar.gz
+gzip -c *.php wp-* license.txt *.html > wp_site.gz
 
 #scp wp_site.gz user@host.com:/path/to/wordpress
